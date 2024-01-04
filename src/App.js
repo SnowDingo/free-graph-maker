1427f7e30697f804b3c401a95d4d3b7bc604624a
SnowDingo <Snowsable113@icloud.com>
2024-01-04 18:18:11 +0900

    made download button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import { Chart } from 'react-chartjs-2';
 import './App.css';
 import { useState } from 'react';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js";
+import { Chart } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+function download(){
+  var canvas = document.getElementById("canvas")
+  var link = document.createElement("a");
+  link.href = canvas.toDataURL("image/png");
+  link.download = "graph.png";
+  link.click();
+}
 function App() {
   const [graphtype,setgraphType] = useState("bar")
   const [data,setdata] = useState([102,92,54,83,132,81,9])
@@ -13,7 +39,7 @@ function App() {
   function ApplyChanges(){
     var graph = graphtype
     console.log(graph)
-    setgraphType("line")
+    setgraphType(graph)
   }
   var graphData= {
     type: [
@@ -30,6 +56,13 @@ function App() {
       "Jun",
       "Jly",
     ],
+    options: {
+      plugins: {
+        customCanvasBackgroundColor: {
+          color: 'white',
+        }
+      }
+    },
     datasets: [
       // 表示するデータセット
       {
@@ -45,8 +78,14 @@ function App() {
         <h1>FreeGraphMaker</h1>
         <div className='options'>
           <h2>ChartContext</h2>
-          <input placeholder={data[0]} onChange={(e) => changedata(e,0)}></input>
-          <input placeholder={data[1]} onChange={(e) => changedata(e,1)}>{}</input>
+            <input placeholder={data[0]} onChange={(e) => changedata(e,0)}>{}</input>
+            <input placeholder={data[1]} onChange={(e) => changedata(e,1)}></input>
+            <input placeholder={data[0]} onChange={(e) => changedata(e,2)}>{}</input>
+            <input placeholder={data[1]} onChange={(e) => changedata(e,3)}>{}</input>
+            <input placeholder={data[0]} onChange={(e) => changedata(e,4)}>{}</input>
+            <input placeholder={data[1]} onChange={(e) => changedata(e,5)}>{}</input>
+            <input placeholder={data[0]} onChange={(e) => changedata(e,6)}>{}</input>
+            <input placeholder={data[1]} onChange={(e) => changedata(e,7)}>{}</input>
           <button onClick={(e) => {ApplyChanges()}}>Apply Changes</button>
           <h2>Graphtype</h2>
           <select id="graphtype" onChange={(e) => {setgraphType(e.target.value)}}>
@@ -55,7 +94,8 @@ function App() {
                   <option value="pie">pie</option>
             </select>
         </div>
-        <Chart className='bar' type={graphtype} data={graphData} redraw ></Chart>
+        <Chart datasetIdKey='key' className='bar' id="canvas" type={graphtype} data={graphData} redraw ></Chart>
+        <button id="download" onClick={(e) => {download()}}>download</button>
       </body>
     </div>
   );
